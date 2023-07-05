dfa67d9a4597ccbcd34f4fdabe8d0b9cc4ba7f1b
codewithnadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2023-07-06 02:41:18 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
       <h5>hello I'm</h5>
       <h1>Mohd Nadeem</h1>
-      <h5 className="text-light">Frontend Developer</h5>
+      <h5 className="text-light">Full Stack Developer</h5>
       <CTA/>
       <HeaderSocials/>
       <div className="me">
